4c131b8a891e6d93ec5a6e3d7ebe3461f630de3f
nguyenphuthinh1025 <nguyenphuthinh1025@gmail.com>
2023-07-10 19:26:08 +0700

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,27 @@
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
+import reduxThunk from 'redux-thunk';
+import { AchivementReducer } from './reducers/AchivementReducer';
+import { ProcessTypeReducer } from './reducers/ProcessTypeReducer';
+import { ReportType } from './reducers/ReportType'
+import { RoleReducer } from './reducers/RoleReducer';
+import { ActivityReducer } from './reducers/ActivityReducer';
+import { baiTapGameXucXacReducer } from './reducers/baiTapGameXucXacReducer';
+import { ConfigActivityReducer } from './reducers/ConfigActivityReducer';
+import { FanpageReducer } from './reducers/FanpageReducer';
+
+
+const rootReducer = combineReducers({
+    AchivementReducer,
+    ProcessTypeReducer,
+    ReportType,
+    RoleReducer,
+    ActivityReducer,
+    baiTapGameXucXacReducer,
+    ConfigActivityReducer,
+    FanpageReducer,
+})
+
+let middleWare = applyMiddleware(reduxThunk);
+let composeCustom = compose(middleWare, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export const store = createStore(rootReducer, composeCustom);
