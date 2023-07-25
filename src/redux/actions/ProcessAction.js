e74f20e5ac914d5922cbeae8f79166e56a99ff1d
nguyenphuthinh1025 <nguyenphuthinh1025@gmail.com>
2023-07-25 11:31:55 +0700

    update 25/7

--- src/redux/actions/ProcessAction.js
+++ src/redux/actions/ProcessAction.js
@@ -15,6 +15,21 @@ export const CreateProcessAction = (value) => {
 
 }
 
+export const CreateProcess1Action = (value, id) => {
+    return async (dispatch) => {
+        try {
+            let result = await http.post('/Process/Insert-process', value);
+            console.log(result.data);
+            // const action = GetListActivityAction()
+            // dispatch(action)
+            const action1 = GetProcessByActivityAction(id)
+            dispatch(action1)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+}
 
 export const GetProcessByActivityAction = (id) => {
     return async (dispatch) => {
